20d357b192e241ee3d91598998b227f34f761291
Andy Le <4ndyle@gmail.com>
2024-09-23 01:18:06 -0700

    initial commit

---
+++ life.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define NUM_ROWS 5
+#define NUM_COLS 5
+
+typedef enum {F=0,T=1} Bool;
+
+int getInitialState(int init_state[]) {
+  
+}
+
+void setInitialState(char board[][NUM_COLS], int init_state[], int num_alive) {
+  
+}
+
+int countLiveNeighbors(char board[][NUM_COLS], int r, int c) {
+ 
+}
+
+void NextGeneration(char board1[][NUM_COLS]) {
+  
+}
+
+void printBoard(char board[][NUM_COLS]) {
+
+}
+
+void PlayGameOfLife() {
+
+}
+
+int main() {
+ 
+}
